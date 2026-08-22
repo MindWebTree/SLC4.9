f451888cf72c556232017b04582525d79929cf54
MindWebTree <82255369+MindWebTree@users.noreply.github.com>
2026-08-22 11:19:26 +0530

    Checkout Payment Method Model added nw prop
    Checkout Payment Method Model added nw prop

---
+++ Presentation/Nop.Web/Models/Customizations/Checkout/CheckoutPaymentInfoModel.cs
@@ -0,0 +1,9 @@
+﻿using Nop.Web.Framework.Models;
+namespace Nop.Web.Models.Checkout
+{
+    public partial record CheckoutPaymentInfoModel : BaseNopModel
+    {
+        public dynamic AdditionaData { get; set; }
+    }
+}
+
