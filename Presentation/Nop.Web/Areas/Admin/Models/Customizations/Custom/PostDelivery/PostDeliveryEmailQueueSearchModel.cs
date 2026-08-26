d168e1b95728dba34f0d83b0dac9baa1f5a33a8f
MindWebTree <info@mindwebtree.com>
2026-08-26 20:47:57 +0530

    Removed Models Which already existed in Mwt plugins

--- Presentation/Nop.Web/Areas/Admin/Models/Customizations/Custom/PostDelivery/PostDeliveryEmailQueueSearchModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nop.Web.Framework.Models;
-
-namespace Nop.Web.Areas.Admin.Models.Customization.Custom.PostDelivery
-{
-    public partial record PostDeliveryQueueEmailSearchModel : BaseSearchModel
-    {
-        public int  Id { get; set; }
-    }
-}
