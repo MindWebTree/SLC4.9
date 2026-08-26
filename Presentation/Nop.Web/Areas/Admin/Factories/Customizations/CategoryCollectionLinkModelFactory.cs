824c90c2dc39f4a838e4a71a52488cf418e2e55d
MindWebTree <info@mindwebtree.com>
2026-08-26 17:31:06 +0530

    default commit --- categorycollection factory added

---
+++ Presentation/Nop.Web/Areas/Admin/Factories/Customizations/CategoryCollectionLinkModelFactory.cs
@@ -0,0 +1,64 @@
+﻿using Nop.Services.Catalog;
+using Nop.Web.Areas.Admin.Models.Catalog;
+using Nop.Web.Framework.Models.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nop.Web.Areas.Admin.Factories
+{
+    public partial class CategoryCollectionLinkModelFactory : ICategoryCollectionLinkModelFactory
+    {
+        #region fields
+
+        private readonly ICategoryCollectionLinkService _categoryCollectionLinksService;
+
+        #endregion
+
+        #region Ctor
+        
+        public CategoryCollectionLinkModelFactory(ICategoryCollectionLinkService categoryCollectionLinksService)
+        {
+            _categoryCollectionLinksService = categoryCollectionLinksService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<CategoryCollectionLinkListModel> PrepareCategoryCollectionLinkListModelAsync(CollectionLinkSearchModel searchModel, int entityId)
+        {
+            if (searchModel == null)
+                throw new ArgumentNullException(nameof(searchModel));
+
+
+            //get collection products
+            var categoryCollectionLinkList = (await _categoryCollectionLinksService
+                .GetCategoryCollectionLinkByEntityId(entityId: entityId)).ToPagedList(searchModel);
+
+            //prepare grid model
+            var model = await new CategoryCollectionLinkListModel().PrepareToGridAsync(searchModel, categoryCollectionLinkList, () =>
+            {
+                return categoryCollectionLinkList.SelectAwait(async categoryCollectionLink=>
+                {
+                    CategoryCollectionLinkModel categoryCollectionLinkModel = new CategoryCollectionLinkModel();
+                    categoryCollectionLinkModel.DisplayOrder = categoryCollectionLink.DisplayOrder;
+                    categoryCollectionLinkModel.EntityId = categoryCollectionLink.EntityId;
+                    categoryCollectionLinkModel.Id = categoryCollectionLink.Id;
+                    categoryCollectionLinkModel.Link = categoryCollectionLink.Link;
+                    categoryCollectionLinkModel.Title = categoryCollectionLink.Title;
+                    categoryCollectionLinkModel.CreatedOnUtc = categoryCollectionLink.CreatedOnUtc;
+                    categoryCollectionLinkModel.UpdatedOnUtc = categoryCollectionLink.UpdatedOnUtc;
+
+                    //fill in additional values (not existing in the entity)
+
+                    return categoryCollectionLinkModel;
+                });
+            });
+            return model;
+        }
+
+        #endregion
+    }
+}
